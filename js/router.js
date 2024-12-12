20c9e1835b65d05f88565660fe37f1024151600c
ryzaer <ryzaer@live.com>
2024-12-12 14:35:51 +0700

    first commit

---
+++ js/router.js
@@ -0,0 +1,35 @@
+const mainRoot = window.location.pathname;
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const handleLocation = async () => {    
+    var   path = window.location.pathname,
+          part = path.split("/"),
+          tags = "";
+          part = part[part.length - 1].trim();
+    const page = part ? part : 'index';
+          if(part)
+            tags = page.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+                return letter.toUpperCase();
+            }) + " ~ ";
+          document.title =  `${tags}Vanilla SPA Router`;
+
+    const urls = part ? path.replace(page, `pages/${page}.html`) : `${path}/pages/index.html`;
+    // const route = routes[path] || routes[`${rooter}/404`];
+    const html = await fetch(urls).then((data) => data.text());
+
+    document.getElementById("main-page").innerHTML = html.split(/<(\/)?template>/ig)[2];
+    // console.log(handleHashChange(1));
+};
+
+const handleHashChange = (ints) => {
+    const hashData = window.location.hash.split("#");
+    return ints > 1 ? hashData[ints] : hashData[1];
+}
+window.onpopstate = handleLocation;
+window.onload = handleLocation;
+window.route = route;
