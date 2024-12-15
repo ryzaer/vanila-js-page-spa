2015d283d2f34bb0a7afa175760556006b68b406
ryzaer <ryzaer@live.com>
2024-12-15 12:38:56 +0700

    up htaccess

--- js/app.js
+++ js/app.js
@@ -2,31 +2,54 @@ const   siteName = "Vanilla SPA Router",
         route = (event) => {
             event = event || window.event;
             event.preventDefault();
-            window.history.pushState({}, "", event.target.href);
+            window.history.pushState({}, "", event.target.href);            
             handleLocation();
         },
-        handleLocation = async () => {    
+        handleLocation = async () => { 
+            const mainElement = document.querySelector("main");   
             var tags = "",
                 path = window.location.pathname,
                 part = path.split("/"),
-                part = part[part.length - 1].trim();
-            const page = part ? part : 'index';
+                part = part[part.length - 1].trim(),
+                page = part ? part : 'index';
+            /** make clear that content is not the same */
+            if(mainElement.getAttribute("content") !== page){
+                var urls = part ? path.replace(page, `pages/${page}.html`) : `${path}/pages/index.html`,
+                    html = await fetch(urls).then((data) => data.text()),
+                    htmc;
+                mainElement.setAttribute('content',page);
+                /** handle error page 4** to 5** */
+                if(html.match(/<title>(\s+)?(4|5)\d{1,2}\s/)){
+                    html = await fetch(urls.replace(page,'404')).then((data) => data.text());
+                    page = '404 ' +  page + ' Page Not Found';
+                }
+                htmc = html.split(/(\n)?<(\/)?template>(\n)?/ig)[4];
+                mainElement.innerHTML = htmc;                
                 if(part)
                     tags = page.toLowerCase().replace(/\b[a-z]/g, function(letter) {
                         return letter.toUpperCase();
                     }) + " ~ ";
                 document.title = tags+siteName;
-
-            const urls = part ? path.replace(page, `pages/${page}.html`) : `${path}/pages/index.html`;
-            // const route = routes[path] || routes[`${rooter}/404`];
-            const html = await fetch(urls).then((data) => data.text());
-            document.querySelector("main").innerHTML = html.split(/(\n)?<(\/)?template>(\n)?/ig)[4];
-            // console.log(handleHashChange(1));
+                /** this is handling hashtags */
+                if(handleHashChange(1)){
+                    var getIDElement = document.getElementById(handleHashChange(1));
+                    !getIDElement || window.scrollTo(0, getIDElement.offsetTop);
+                }
+            }
         },
         handleHashChange = (ints) => {
             const hashData = window.location.hash.split("#");
             return ints > 1 ? hashData[ints] : hashData[1];
-        }
+        };
 window.onpopstate = handleLocation;
 window.onload = handleLocation;
 window.route = route;
+document.addEventListener('click', function(event) {
+    // Check if the clicked element is an <a> tag
+    const anchor = event.target.closest('a');    
+    if (anchor){
+        const href = anchor.getAttribute('href');
+        if (anchor.hasAttribute('href') && !anchor.hasAttribute('target') && !href.startsWith('#')) 
+            route(event)
+    }
+});
